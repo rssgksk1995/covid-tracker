672343e247f10ad429f91ad462d5c097da61f2fc
Gaurav <gaurav.singh@codemonk.in>
2021-01-02 00:14:44 +0530

    'dist'

---
+++ dist/precache-manifest.ee7541fd91f63c1cb2096457ce8ae85e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f1d6e011d6899ad70aac85db3e3f7b",
+    "url": "/covid19.png"
+  },
+  {
+    "revision": "1551aaff83a1df9ea832",
+    "url": "/css/chunk-38868810.bf9cf667.css"
+  },
+  {
+    "revision": "01ef2242d0ca5a92cd2d",
+    "url": "/css/chunk-75cb10a0.6493925e.css"
+  },
+  {
+    "revision": "50b7c43c4f67645be10c",
+    "url": "/css/chunk-7e180ee0.d4cf98ea.css"
+  },
+  {
+    "revision": "940f5b63db291792624e",
+    "url": "/css/chunk-cb04682e.9e338e87.css"
+  },
+  {
+    "revision": "92c342a883bff432b4e0",
+    "url": "/css/chunk-f46af166.74e2c258.css"
+  },
+  {
+    "revision": "51a808b819421f43480a",
+    "url": "/css/chunk-vendors.b6c9e17b.css"
+  },
+  {
+    "revision": "8015d764fe1a5f2761f50714778eede4",
+    "url": "/img/home.8015d764.svg"
+  },
+  {
+    "revision": "1380a5bfae9a691ac5b357d5cff8f667",
+    "url": "/img/trello.1380a5bf.svg"
+  },
+  {
+    "revision": "086ab2c5c0d09b6c768ed7a648e6cff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6678f0d3422b1d727be",
+    "url": "/js/app.ab1e00db.js"
+  },
+  {
+    "revision": "1551aaff83a1df9ea832",
+    "url": "/js/chunk-38868810.77be1f20.js"
+  },
+  {
+    "revision": "01ef2242d0ca5a92cd2d",
+    "url": "/js/chunk-75cb10a0.d7c425a7.js"
+  },
+  {
+    "revision": "50b7c43c4f67645be10c",
+    "url": "/js/chunk-7e180ee0.e99dfb36.js"
+  },
+  {
+    "revision": "940f5b63db291792624e",
+    "url": "/js/chunk-cb04682e.91ecd9cb.js"
+  },
+  {
+    "revision": "92c342a883bff432b4e0",
+    "url": "/js/chunk-f46af166.9334413b.js"
+  },
+  {
+    "revision": "51a808b819421f43480a",
+    "url": "/js/chunk-vendors.a17ce06b.js"
+  },
+  {
+    "revision": "64c62869a73f01a94274e4dfea392c15",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
