e7c169c4051a64877f732c4c666eec5fc65252af
Gaurav <gaurav.singh@codemonk.in>
2021-01-01 19:46:02 +0530

    'deploy'

---
+++ dist/precache-manifest.765b4346f2cc99719cbb9bdfc0395656.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8760f9596a24eaa6983",
+    "url": "/css/chunk-19e0cd64.468556e5.css"
+  },
+  {
+    "revision": "c3405bcfb22f656c8baf",
+    "url": "/css/chunk-1b30187b.303f18bf.css"
+  },
+  {
+    "revision": "8f22099649f2507047cc",
+    "url": "/css/chunk-dc6362d4.1270ef02.css"
+  },
+  {
+    "revision": "bf5a0fcb9ac6c12226eb",
+    "url": "/css/chunk-e02517aa.57697ff5.css"
+  },
+  {
+    "revision": "05e22f92097373d9ccf4",
+    "url": "/css/chunk-vendors.b6c9e17b.css"
+  },
+  {
+    "revision": "8015d764fe1a5f2761f50714778eede4",
+    "url": "/img/home.8015d764.svg"
+  },
+  {
+    "revision": "1380a5bfae9a691ac5b357d5cff8f667",
+    "url": "/img/trello.1380a5bf.svg"
+  },
+  {
+    "revision": "0ed36e39dc909ac7beae77192c851c37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77fddf8a9d16f6e84f49",
+    "url": "/js/app.65a8c289.js"
+  },
+  {
+    "revision": "a8760f9596a24eaa6983",
+    "url": "/js/chunk-19e0cd64.0849b212.js"
+  },
+  {
+    "revision": "c3405bcfb22f656c8baf",
+    "url": "/js/chunk-1b30187b.55861d00.js"
+  },
+  {
+    "revision": "8f22099649f2507047cc",
+    "url": "/js/chunk-dc6362d4.16c289ab.js"
+  },
+  {
+    "revision": "bf5a0fcb9ac6c12226eb",
+    "url": "/js/chunk-e02517aa.07b6031f.js"
+  },
+  {
+    "revision": "05e22f92097373d9ccf4",
+    "url": "/js/chunk-vendors.19ed21a7.js"
+  },
+  {
+    "revision": "64c62869a73f01a94274e4dfea392c15",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
